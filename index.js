584fd352ecd8a537e740d6d5e5d36b9b3a462b83
miresque <mirkilinc@gmail.com>
2022-08-04 16:31:12 +0100

    Core Criteria met

--- index.js
+++ index.js
@@ -10,7 +10,31 @@ app.use(express.json())
 
 // Retrieve Current Counter
 app.get("/counter", (req, res) => {
-    res.status(200).send({...counter})
+    res.status(200).send(counter)
+})
+
+// Reset the counter to 0
+app.delete("/counter", (req, res) => {
+    counter.counter = 0
+    res.status(200).send(counter)
+})
+
+// Increment the counter
+app.post("/counter/increment", (req, res) => {
+    counter.counter += 1
+    res.status(201).send(counter)
+})
+
+// Decrement the counter
+app.post("/counter/decrement", (req, res) => {
+    counter.counter -= 1;
+    res.status(201).send(counter)
+})
+
+// Double the counter
+app.post("/counter/double", (req, res) => {
+    counter.counter *= 2;
+    res.status(201).send(counter)
 })
 
 //Start up our server
